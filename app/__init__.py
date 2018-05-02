aed00b0b96c27155c8f5fd08c45e0f722afadf89
bak45247 <bak45247@bethel.edu>
2018-05-02 11:20:03 -0500

    final step in making it run

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,13 @@ import jinja2
 
 from flask import Flask, render_template, request
 import datetime
+
 from app import person, query
+
 app = Flask(__name__)
+app.config.from_object('config')
+
+from app.db import db_functions as db
 
 # Shows the year for the template
 app.jinja_env.globals.update(now=datetime.datetime.now())
