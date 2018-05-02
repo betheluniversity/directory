fd511b3f9411657dd381ca8459be4b8609f234aa
bak45247 <bak45247@bethel.edu>
2018-05-02 11:08:58 -0500

    Steps to making it run

---
+++ app/db/db_functions.py
@@ -0,0 +1,30 @@
+from app.db.db_connection_bw import conn_bw
+
+
+def get_results(result, label="", type=None):
+    ret = {}
+    for i, row in enumerate(result):
+        row_dict = {}
+        for item in row:
+            if isinstance(item, str) or isinstance(item, unicode):
+                item = item.split(":", 1)
+            else:
+                # blob
+                item = item.read()
+            if len(item) > 1:
+                row_dict[item[0]] = item[1]
+            else:
+                # if the result doesn't have key/value pairs
+                # use a custom label
+                row_dict[label] = item[0]
+
+        ret[int(i)] = row_dict
+    return ret
+
+
+def portal_profile(username):
+    call_cursor_bw = conn_bw.cursor()
+    result_cursor_bw = conn_bw.cursor()
+    call_cursor_bw.callproc('bth_portal_channel_api.bu_profile', (username, result_cursor_bw,))
+    r = result_cursor_bw.fetchall()
+    return get_results(r)
