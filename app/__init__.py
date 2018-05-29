0bf627efabdecaa2ceaad7c5b7eb9201efc0a4f6
bak45247 <bak45247@bethel.edu>
2018-05-29 11:43:23 -0500

    small fixes

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,5 @@ app.jinja_env.globals.update(now=datetime.datetime.now())
 from app.views.home import HomeView
 HomeView.register(app, route_base='/')
 
-
 from app.views.results import ResultsView
 ResultsView.register(app)
