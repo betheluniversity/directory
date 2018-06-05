916195fdb7e39e765bcd5abadde968cc9acd909f
bak45247 <bak45247@bethel.edu>
2018-06-05 14:44:34 -0500

    fixed everything

--- app/__init__.py
+++ app/__init__.py
@@ -1,68 +1,15 @@
-import jinja2
-
-from flask import Flask, render_template, request
-import datetime
-from app import person, query
-from app.person import Person
-from app.query import Query
+from flask import Flask
+from flask.ext.cache import Cache
 
 app = Flask(__name__)
+app.config.from_object('config')
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+## TODO: Production cache
 
-# Shows the year for the template
-app.jinja_env.globals.update(now=datetime.datetime.now())
+from app.db import db_functions as db
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+from app.views.home import HomeView
+HomeView.register(app, route_base='/')
 
-
-@app.route("/results")
-def results():
-    repeat = 10
-    return render_template('results.html', repeat=repeat)
-
-
-@app.route("/search", methods=["POST"])
-def search():
-    data = request.form
-    new_person = Person()
-    # The Query
-    search_for = query.Query(data["firstName"], data['lastName'])
-
-    if "Students" in data:
-        # search_for.set_student(data['Students'])
-        new_person.student = data['Students']
-    #TODO Fix the following blocks
-    if "Pictures" in data:
-        # search_for.__set__("picture", data['Students'])
-        new_person.picture = ("picture", data['Student'])
-    if "Groups" in data:
-        # search_for.__set__("groups", data['Students'])
-        new_person.groups = ("groups", data['Students'])
-    if "Home" in data:
-        # search_for.__set__("home", data['Students'])
-        new_person.home = ("home", data['Students'])
-    if "Staff" in data:
-        # search_for.set_teacher(data['Staff'])
-        new_person.teacher = data['Staff']
-
-    people = make_dummy()
-    students = []
-    faculty = []
-    for user in people:
-        if search_for.student == "Students":
-            if user.role == "student":
-                students.append(user)
-        if search_for.teacher == "Staff":
-            if user.role == "faculty":
-                faculty.append(user)
-    return render_template('results.html', students=students, faculty=faculty, query=search_for)
-
-
-def make_dummy():
-    people = []
-    for i in range(3):
-        people.append(person.Person("Boston", "Knighton-Johnson", "bak45247@bethel.edu", "Heritage Hall 105A", 1368, "student", "https://bsp-nas-dav.bethel.edu/IDCentre/Photos/51189.jpg"))
-        people.append(person.Person("Jay", "Barnes", "j-barnes@bethel.edu", "CLC 234", 2372, "faculty", "https://bsp-nas-dav.bethel.edu/IMAGES/CARS/CARS/20010905/13145000.JPG"))
-        people.append(person.Person("Eric", "Jameson", "e-jameson@bethel.edu", "St. Paul", 2355, "faculty", "https://bsp-nas-dav.bethel.edu/IDCentre/Photos/36897.jpg"))
-    return people
+from app.views.results import ResultsView
+ResultsView.register(app)
