929f07329cc87200a259a49f2c64108e6b70ba46
c-schwarze <ces55739@bethel.edu>
2019-01-22 17:10:27 -0600

    Added in "ITS - Employees" role from ldap

--- app/directory_controller.py
+++ app/directory_controller.py
@@ -1,9 +1,13 @@
-import time
+import ldap
+import re
 import requests
+import time
+
 
 from flask import session, request, current_app
 from flask import json as fjson
 
+from app import app
 from app.db.db_functions import portal_profile
 
 
@@ -13,15 +17,19 @@ class DirectoryController(object):
 
     def before_request(self):
         def init_user():
+            if 'session_time' in session.keys():
+                seconds_in_12_hours = 60 * 60 * 12  # equates to 12 hours
+                reset_session = time.time() - session['session_time'] >= seconds_in_12_hours
+            else:
+                reset_session = True
+
             dev = current_app.config['ENVIRON'] != 'prod'
 
-            # reset session if it has been more than 24 hours
-            if 'session_time' in session.keys():
-                seconds_in_day = 60 * 60 * 24
-                day_is_passed = time.time() - session['session_time'] >= seconds_in_day
-            else:
-                day_is_passed = True
-                session['session_time'] = time.time()
+            # if not production or 12 hours have past, then clear our session variables on each call
+            if dev or reset_session:
+                session.clear()
+                seconds_in_12_hours = 60 * 60 * 12
+                session['session_time'] = time.time() + seconds_in_12_hours
 
             if 'username' not in session.keys():
                 get_user()
@@ -32,8 +40,8 @@ class DirectoryController(object):
             if 'profile' not in session.keys():
                 session['profile'] = portal_profile(session['username'])
 
-            if 'all_id' not in session.keys():
-                show_all_id()
+            if 'ITS_view' not in session.keys():
+                get_its_view()
 
         def get_user():
             if current_app.config['ENVIRON'] == 'prod':
@@ -57,11 +65,23 @@ class DirectoryController(object):
 
             return ret
 
-        def show_all_id():
-            session['all_id'] = 'false'
-            for role in session['roles']:
-                if 'staff' in role:
-                    session['all_id'] = 'true'
-                # here is the place for the ITS role check
+        def get_its_view():
+            try:
+                session['ITS_view'] = False
+                con = ldap.initialize(app.config['LDAP_CONNECTION_INFO'])
+                con.simple_bind_s('BU\svc-tinker', app.config['LDAP_SVC_TINKER_PASSWORD'])
+
+                # code to get all users in a group
+                results = con.search_s('ou=Bethel Users,dc=bu,dc=ac,dc=bethel,dc=edu', ldap.SCOPE_SUBTREE,
+                                       "(|(&(sAMAccountName=%s)))" % session['username'])
+
+                for result in results:
+                    for ldap_string in result[1]['memberOf']:
+                        user_iam_group = re.search('CN=([^,]*)', str(ldap_string)).group(1)
+                        if user_iam_group == 'ITS - Employees':
+                            session['ITS_view'] = True
+            except:
+                session['ITS_view'] = False
+
 
         init_user()
