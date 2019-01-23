41a93827f84756d7eba2e35eac605e2ed4df4dc3
Boston KJ <bak45247@bethel.edu>
2019-01-23 14:53:24 -0600

    redirected forms to their own respective searches
    moved some other functions to their own methods

--- app/views/home.py
+++ app/views/home.py
@@ -21,207 +21,157 @@ class View(FlaskView):
         return render_template('profile.html', **locals())
 
     @route('/', methods=['POST'])
-    def passage(self):
-        data = self.get_data(request.form.to_dict())
-
-        if data['home'] == 'home':
-            home = True
-        if data['group'] == 'group':
-            group = True
-
-        if data['first_name'] != '' or data['last_name'] != '':  # checking which people to show for name search
-            if data['faculty'] != '' and data['student'] == '':
-                option = 'faculty'  # showing just staff/faculty results
-            elif data['student'] != '' and data['faculty'] == '':
-                option = 'student'  # showing just student results
-            else:
-                option = 'both'  # showing all results
-
-            result = self.fl_search(data['first_name'], data['last_name'], option)
-
-        elif data['username'] != '':
-            result = self.username_search(data['username'])
-
-        elif data['email'] != '':
-            result = self.email_search(data['email'])
-
-        elif data['department'] != '':
-            group = True
-            result = self.dept_search(data['department'])
-
-        elif data['bu_id'] != '':
-            group = True
-            result = self.id_search(data['bu_id'])
-
-        depts = departments() # so we can use the autocompleter in the department search after reloading the page
-        return render_template('results.html', **locals())
-
-    def get_data(self, data):  # method to ensure we have data in all variables
-        # Bruteforcing this because I HAVE to try/except every. single. line.
-        # If BadRequestKeyErrors can be mass checked, I don't know how
-        try:
-            data['home'] = data['home']
-        except (BadRequestKeyError, KeyError):
-            data['home'] = ''
-        try:
-            data['group'] = data['group']
-        except (BadRequestKeyError, KeyError):
-            data['group'] = ''
-        try:
-            data['student'] = data['student']
-        except (BadRequestKeyError, KeyError):
-            data['student'] = ''
-        try:
-            data['faculty'] = data['faculty']
-        except (BadRequestKeyError, KeyError):
-            data['faculty'] = ''
-
-        # second part is the fields from the forms
-        try:
-            data['first_name'] = data['first_name']
-        except (BadRequestKeyError, KeyError):
-            data['first_name'] = ''
-        try:
-            data['last_name'] = data['last_name']
-        except (BadRequestKeyError, KeyError):
-            data['last_name'] = ''
-        try:
-            data['username'] = data['username']
-        except (BadRequestKeyError, KeyError):
-            data['username'] = ''
-        try:
-            data['email'] = data['email']
-        except (BadRequestKeyError, KeyError):
-            data['email'] = ''
-        try:
-            data['department'] = data['department']
-        except (BadRequestKeyError, KeyError):
-            data['department'] = ''
-        try:
-            data['bu_id'] = data['bu_id']
-        except (BadRequestKeyError, KeyError):
-            data['bu_id'] = ''
-
-        return data
+    def get_option(self, data):
+        if hasattr(data, 'faculty') and hasattr(data, 'student'):
+            return 'both'  # showing all results
+        elif hasattr(data, 'faculty'):
+            return 'faculty'  # showing just staff/faculty results
+        elif hasattr(data, 'student'):
+            return 'student'  # showing just student results
+        else:
+            return 'both'  # defaults to showing all results
 
     # first and last name search. Holds the details and logic surrounding the first and last name searches
-    def fl_search(self, first_name, last_name, option):  # option is the advanced settings for student/staff
+    @route('/', methods=['POST'])
+    def fl_search(self):  # option is the advanced settings for student/staff
+        data = request.form.to_dict()
+        option = self.get_option(data)
+
         people = directory_search()
         result = []
 
-        if first_name != '' and last_name != '':  # If both boxes are filled out, this will be the loop that is checked
+        if data['first_name'] != '' and data['last_name'] != '':  # If both boxes are filled out, this will be the loop that is checked
             for row in people:
-                check = False
-                if option == 'both':
-                    check = True
-                else:
-                    for role in row['role']:
-                        if role == option:
-                            check = True
-                if check:
-                    ratio = (self.fl_fuzzy(row['first_name'], row['last_name'], True, first_name) +
-                             self.fl_fuzzy(row['first_name'], row['last_name'], False, last_name))
+                if self.match_option(row, option):
+                    ratio = (self.fl_fuzzy(row['first_name'], row['last_name'], True, data['first_name']) +
+                             self.fl_fuzzy(row['first_name'], row['last_name'], False, data['last_name']))
 
                     if ratio >= 75:
                         self.make_results(row, result, ratio)
 
-        elif first_name != '' and last_name == '':  # called if first name and NOT last name are filled out
+        elif data['first_name'] != '' and data['last_name'] == '':  # called if first name and NOT last name are filled out
             for row in people:
-                check = False
-                if option == 'both':
-                    check = True
-                else:
-                    for role in row['role']:
-                        if role == option:
-                            check = True
-                if check:
-                    ratio = self.fl_fuzzy(row['first_name'], row['last_name'], True, first_name)
+                if self.match_option(row, option):
+                    ratio = self.fl_fuzzy(row['first_name'], row['last_name'], True, data['first_name'])
                     if ratio >= 75:
                         self.make_results(row, result, ratio)
 
-        elif last_name != '' and first_name == '':  # called if last name and NOT first name are filled out
+        elif data['last_name'] != '' and data['first_name'] == '':  # called if last name and NOT first name are filled out
             for row in people:
-                check = False
-                if option == 'both':
-                    check = True
-                else:
-                    for role in row['role']:
-                        if role == option:
-                            check = True
-                if check:
-                    ratio = self.fl_fuzzy(row['first_name'], row['last_name'], False, last_name)
+                if self.match_option(row, option):  # if its true, check the person
+                    ratio = self.fl_fuzzy(row['first_name'], row['last_name'], False, data['last_name'])
                     if ratio >= 75:
                         self.make_results(row, result, ratio)
+
+        elif data['last_name'] == '' and data['first_name'] == '':  # if both are empty, return everyone
+            result = people
+            return render_template('results.html', **locals())
 
         result.sort(key=lambda i: i['last_name'])
         result.sort(key=lambda i: i['ratio'], reverse=True)
 
-        return result
+        depts = departments()  # this will be removed once the ajax is implemented
+        return render_template('results.html', **locals())
 
     # Username search executes, creates, and formats the username searches
-    def username_search(self, username):
+    def username_search(self):
+        data = request.form.to_dict()
+        option = self.get_option(data)
+
         people = directory_search()
         result = []
 
-        if username != '':
+        if data['username'] != '':  # put in the student/staff filters
             for row in people:
-                ratio = self.other_fuzzy(username, row['username'])
-                if ratio > 75:
-                    self.make_results(row, result, ratio)
+                if self.match_option(row, option):
+                    ratio = self.misc_fuzzy(data['username'], row['username'])
+                    if ratio > 75:
+                        self.make_results(row, result, ratio)
 
         result.sort(key=lambda i: i['last_name'])
         result.sort(key=lambda i: i['ratio'], reverse=True)
 
-        return result
+        depts = departments()  # this will be removed once the ajax is implemented
+        return render_template('results.html', **locals())
 
     # Email search, executes, creates, and formats the email search and results
-    def email_search(self, email):
+    def email_search(self):
+        data = request.form.to_dict()
+        option = self.get_option(data)
+
         people = directory_search()
         result = []
 
-        if email != '':
+        if data['email'] != '':  # put in the student/staff filters
             for row in people:
-                ratio = self.other_fuzzy(email, row['email'])
-                if ratio > 75:
-                    self.make_results(row, result, ratio)
+                if self.match_option(row, option):
+                    ratio = self.misc_fuzzy(data['email'], row['email'])
+                    if ratio > 75:
+                        self.make_results(row, result, ratio)
+        else:
+            result = people
+            return render_template('results.html', **locals())
 
         result.sort(key=lambda i: i['last_name'])
         result.sort(key=lambda i: i['ratio'], reverse=True)
 
-        return result
+        depts = departments()  # necessary until we build the ajax call for this
+        return render_template('results.html', **locals())
 
     # department search, subject to change
-    def dept_search(self, department):
+    def dept_search(self):
+        data = request.form.to_dict()
+        option = self.get_option(data)
+
         people = directory_search()
         result = []
 
-        if department != '':
+        if data['department'] != '':
             for row in people:
-                for item in row['department']:
-                    if department in item:
-                        result.append(row)
+                if self.match_option(row, option):
+                    for item in row['department']:
+                        if data['department'] in item:
+                            result.append(row)
 
         result.sort(key=lambda i: i['last_name'])
         result.sort(key=lambda i: i['id'])
 
-        return result
+        depts = departments()  # necessary until we build the ajax call for this
+        return render_template('results.html', **locals())
 
     # id search, only visible to those whose roles allow it
     # does the same as the other search functions
-    def id_search(self, bu_id):
+    def id_search(self):
+        data = request.form.to_dict()
+        option = self.get_option(data)
+
         people = directory_search()
         result = []
 
-        if bu_id != '':
+        if data['id'] != '':
             for row in people:
-                ratio = self.other_fuzzy(bu_id, row['id'])
-                if ratio > 75:
-                    self.make_results(row, result, ratio)
+                if self.match_option(row, option):
+                    ratio = self.misc_fuzzy(data['id'], row['id'])
+                    if ratio > 75:
+                        self.make_results(row, result, ratio)
 
         result.sort(key=lambda i: i['last_name'])
         result.sort(key=lambda i: i['ratio'], reverse=True)
 
-        return result
+        depts = departments()  # necessary until we build the ajax call for this
+        return render_template('results.html', **locals())
+
+    def match_option(self, row, option, ):
+        match_option = False  # does the selected option match the role of the person?
+        if option == 'both':
+            match_option = True  # if option is both, set to true by default
+        else:
+            for role in row['role']:
+                if role == option:
+                    match_option = True  # if the role of the person matches the selected option, set true
+        if match_option:  # if its true, check the person
+            return True
+        return False
 
     # Fuzzy method for first and last names, contains some extra logic
     def fl_fuzzy(self, first_name, last_name, fl, search):
@@ -243,7 +193,7 @@ class View(FlaskView):
 
         return ratio
 
-    def other_fuzzy(self, search, key):  # much simpler fuzz method to use for things other than first or last name
+    def misc_fuzzy(self, search, key):  # much simpler fuzz method to use for things other than first or last name
         ratio = fuzz.ratio(search, key)
         if search in key:
             ratio = 101
