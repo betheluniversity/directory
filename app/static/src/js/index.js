3fe8b2d1756b8859bff824d43699f3ae6004e66c
c-schwarze <ces55739@bethel.edu>
2019-04-26 11:10:48 -0500

    add in first test

--- app/static/src/js/index.js
+++ app/static/src/js/index.js
@@ -6,6 +6,18 @@
 import '../css/main.scss'
 import '../css/awesomplete.css'
 
+if (!Object.entries) {
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+    return resArray;
+  };
+}
+
 import { } from './ajaxCall'
 import { } from './showHide'
 import { } from 'awesomplete'
