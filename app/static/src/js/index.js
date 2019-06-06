9adfb362de736bf1b3ce5cd10b0b515f0cd01977
Jacob Johnson <johjacb@bethel.edu>
2019-06-06 10:40:12 -0500

    trying polyfill.io

--- app/static/src/js/index.js
+++ app/static/src/js/index.js
@@ -17,16 +17,3 @@ import { } from 'awesomplete'
 window.addEventListener('load', function () {
     document.body.classList.remove('preload')
 })
-
-// Polyfill for customEvent in IE 11
-(function () {
-    if (typeof window.CustomEvent === 'function') return false
-    function CustomEvent (event, params) {
-        params = params || { bubbles: false, cancelable: false, detail: null }
-        var evt = document.createEvent('CustomEvent')
-        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
-        return evt
-    }
-    CustomEvent.prototype = window.Event.prototype
-    window.CustomEvent = CustomEvent
-})()
