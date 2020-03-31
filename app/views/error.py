c1622d8c95c66aff951ecf34039b14c0978963e0
Caleb Schwarze <c-schwarze@bethel.edu>
2020-03-31 10:57:41 -0500

    don't log 403

--- app/views/error.py
+++ app/views/error.py
@@ -18,7 +18,7 @@ def error_render_template(error, code=None):
     # Means that it's a handled error/exception
     if code is not None:
         # As of 11/16/2017, we're only logging 403s and 500 errors.
-        if code == 403 or code > 499:
+        if code > 499:
             sentry_sdk.capture_exception()
             app.logger.error("%s -- %s" % (username, str(error)))
 
